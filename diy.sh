468c45ca44fdc7901aba0f314410edc72ecffb92
darkrain88 <39786175+darkrain88@users.noreply.github.com>
2020-02-10 19:23:59 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,3 +7,7 @@
 #=================================================
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_generate
+sed -i 's/^[ \t]*//g' ./.config
+echo '修改网关地址'
+sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_generate
+make defconfig
